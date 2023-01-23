59f1649656e4e0fe872183f37143676f4b333abe
Timosha <flykaiq449@gmail.com>
2023-01-23 17:38:02 +0300

    fix11

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,10 @@
-import { Sequelize } from 'sequelize'
+import { Sequelize } from 'sequelize';
+import mysql2 from 'mysql2';
 
 const db = new Sequelize('freedb_users-db', 'freedb_timkhab', 'XE@dX$mQR?43DGv', {
     host: 'sql.freedb.tech',
     dialect: 'mysql',
+    dialectModule: mysql2,
 }); 
 
 export default db;
