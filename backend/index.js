9433ce9114388ee2a67d4f13619d90a4426a4f5c
Timosha <flykaiq449@gmail.com>
2023-01-23 18:42:32 +0300

    fix13

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,14 @@ import express from 'express';
 import UserRoute from './routes/UserRoute.js';
 import cors from 'cors';
 
-
 const app = express();
-
-//app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(UserRoute);
 
 console.log('server running');
 
-
-// app.get('/', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
-app.listen(5000);
+const server = http.createServer(app);
+server.listen(5000);
